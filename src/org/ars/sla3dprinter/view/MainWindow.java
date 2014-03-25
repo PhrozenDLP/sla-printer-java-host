cdde25c0ae02566366f9668cca89e1b4c5ea0ff7
JimYTC <solofat@gmail.com>
2014-03-25 22:57:49 +0800

    GUI - step motor control interface

--- src/org/ars/sla3dprinter/view/MainWindow.java
+++ src/org/ars/sla3dprinter/view/MainWindow.java
@@ -1,10 +1,19 @@
 package org.ars.sla3dprinter.view;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.SystemColor;
+import java.awt.Font;
 
 public class MainWindow {
+    private static final int START_POS_X = 100;
+    private static final int START_POS_Y = 100;
+    private static final int WIDTH = 180;
+    private static final int HEIGHT = 80;
 
-    private JFrame frame;
+    private JFrame frmSladPrinter;
 
     /**
      * Create the application.
@@ -17,13 +26,31 @@ public class MainWindow {
      * Initialize the contents of the frame.
      */
     private void initialize() {
-        frame = new JFrame();
-        frame.setBounds(100, 100, 900, 700);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frmSladPrinter = new JFrame();
+        frmSladPrinter.setTitle("SLA 3D Printer");
+        frmSladPrinter.setBounds(START_POS_X, START_POS_Y, 260, 120);
+        frmSladPrinter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frmSladPrinter.getContentPane().setLayout(null);
+        frmSladPrinter.setResizable(false);
+
+        JLabel lblStepMotor = new JLabel("Step Motor control");
+        lblStepMotor.setFont(new Font("Monaco", Font.PLAIN, 16));
+        lblStepMotor.setForeground(SystemColor.controlHighlight);
+        lblStepMotor.setHorizontalAlignment(SwingConstants.CENTER);
+        lblStepMotor.setBounds(6, 6, 246, 29);
+        frmSladPrinter.getContentPane().add(lblStepMotor);
+        
+        JButton btnCwHalfTurn = new JButton("CW half turn");
+        btnCwHalfTurn.setBounds(6, 48, 117, 29);
+        frmSladPrinter.getContentPane().add(btnCwHalfTurn);
+        
+        JButton btnCcwHalfTurn = new JButton("CCW half turn");
+        btnCcwHalfTurn.setBounds(135, 48, 117, 29);
+        frmSladPrinter.getContentPane().add(btnCcwHalfTurn);
+
     }
 
     public void show() {
-        frame.setVisible(true);
+        frmSladPrinter.setVisible(true);
     }
-
 }
