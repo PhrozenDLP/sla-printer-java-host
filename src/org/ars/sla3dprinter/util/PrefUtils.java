adb1c7b48e9d29fa1bcaa9980ffb6489ef5ed852
JimYTC <solofat@gmail.com>
2016-01-23 15:54:13 +0800

    Add preference for delay after action

--- src/org/ars/sla3dprinter/util/PrefUtils.java
+++ src/org/ars/sla3dprinter/util/PrefUtils.java
@@ -12,6 +12,9 @@ public class PrefUtils {
     private static final String PREF_BASELAYER_STEPS_FROM_TOP = "pref_baselayer_steps_from_top";
     private static final int PREF_BASELAYER_STEPS_FROM_TOP_DEFAULT = 120000;
 
+    private static final String PREF_DELAY_AFTER_ACTION = "pref_delay_after_action";
+    public static final int PREF_DELAY_AFTER_ACTION_DEFAULT_MILLIS = 500;
+
     public static Preferences getInstance() {
         if (sPrefs == null) {
             sPrefs = Preferences.userRoot().node(Consts.PREF_NODE_NAME);
@@ -27,4 +30,12 @@ public class PrefUtils {
         return getInstance().getInt(PREF_BASELAYER_STEPS_FROM_TOP, PREF_BASELAYER_STEPS_FROM_TOP_DEFAULT);
     }
 
+    public static void setDelayAfterActionDefaultMillis(int millis) {
+        if (millis > 0) getInstance().putInt(PREF_DELAY_AFTER_ACTION, millis);
+    }
+
+    public static int getDelayAfterActionDefaultMillis() {
+        return getInstance().getInt(PREF_DELAY_AFTER_ACTION,
+                PREF_DELAY_AFTER_ACTION_DEFAULT_MILLIS);
+    }
 }
--- src/org/ars/sla3dprinter/view/PreferenceDialog.java
+++ src/org/ars/sla3dprinter/view/PreferenceDialog.java
@@ -17,32 +17,51 @@ public class PreferenceDialog extends JDialog implements ActionListener {
     public static final String TITLE = "Printer Preference";
 
     private JTextField mInputBaseLayerStepsFromTop;
-    private JButton mBtnOk = new JButton("OK");
+    private JTextField mInputDelayAfterAction;
+    private JButton mBtnOk;
 
     public PreferenceDialog(Frame frame) {
         super(frame, TITLE, true);
         setModal(false);
 
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+
+        // Base layer from top
         JLabel lbBaseLayerStepsFromTop = new JLabel("BaseLayer steps from top:");
         lbBaseLayerStepsFromTop.setFont(Consts.APP_FONT);
+        GridBagConstraints c1 = getGridBagConstraint(0, 0, 1, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER);
+        panel.add(lbBaseLayerStepsFromTop, c1);
 
         int steps = PrefUtils.getBaseLayerStepsFromTop();
         mInputBaseLayerStepsFromTop = new JTextField(Integer.toString(steps));
         mInputBaseLayerStepsFromTop.setFont(Consts.APP_FONT);
         mInputBaseLayerStepsFromTop.setSize(new Dimension(150, 30));
+        GridBagConstraints c2 = getGridBagConstraint(1, 0, 1, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER);
+        panel.add(mInputBaseLayerStepsFromTop, c2);
 
+        // Delay after action
+        JLabel lbDelayAfterAction = new JLabel("Delay after action(ms):");
+        lbBaseLayerStepsFromTop.setFont(Consts.APP_FONT);
+        GridBagConstraints c3 = getGridBagConstraint(0, 1, 1, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER);
+        panel.add(lbDelayAfterAction, c3);
+
+        int millis = PrefUtils.getDelayAfterActionDefaultMillis();
+        mInputDelayAfterAction = new JTextField(Integer.toString(millis));
+        mInputDelayAfterAction.setFont(Consts.APP_FONT);
+        mInputDelayAfterAction.setSize(new Dimension(150, 30));
+        GridBagConstraints c4 = getGridBagConstraint(1, 1, 1, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER);
+        panel.add(mInputDelayAfterAction, c4);
+
+        // Submit button
         mBtnOk = new JButton("OK");
         mBtnOk.setFont(Consts.APP_FONT);
         mBtnOk.setSize(new Dimension(50, 30));
         mBtnOk.addActionListener(this);
+        GridBagConstraints c5 = getGridBagConstraint(1, 2, 1, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER);
+        panel.add(mBtnOk, c5);
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout(0, 5));
-        panel.add(lbBaseLayerStepsFromTop, BorderLayout.NORTH);
-        panel.add(mInputBaseLayerStepsFromTop, BorderLayout.CENTER);
-        panel.add(mBtnOk, BorderLayout.SOUTH);
         getContentPane().add(panel);
-
         setResizable(false);
         pack();
         setLocationRelativeTo(frame);
@@ -51,6 +70,20 @@ public class PreferenceDialog extends JDialog implements ActionListener {
     public void actionPerformed(ActionEvent ev) {
         int steps = Integer.parseInt(mInputBaseLayerStepsFromTop.getText());
         PrefUtils.setBaseLayerStepsFromTop(steps);
+        int millis = Integer.parseInt(mInputDelayAfterAction.getText());
+        PrefUtils.setDelayAfterActionDefaultMillis(millis);
         setVisible(false);
+    }
+
+    private GridBagConstraints getGridBagConstraint(int gridx, int gridy, int gridwidth,
+                                                    int gridheight, int fill, int anchor) {
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = gridx;
+        constraints.gridy = gridy;
+        constraints.gridwidth = gridwidth;
+        constraints.gridheight = gridheight;
+        constraints.fill = fill;
+        constraints.anchor = anchor;
+        return constraints;
     }
 }
